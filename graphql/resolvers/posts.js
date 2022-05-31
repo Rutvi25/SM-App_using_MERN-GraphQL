06858c985b0480bc3db5a017ccd2c7d5292a27ab
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-05-31 15:00:20 +0530

    create & delete posts

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,8 @@
-const Post = require('../../models/Post')
+const { AuthenticationError } = require('apollo-server');
+
+const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
+
 module.exports ={
   Query: {
     async getPosts() {
@@ -6,6 +10,46 @@ module.exports ={
         const posts = await Post.find();
         return posts
       } catch(err) {
+        throw new Error(err);
+      }
+    },
+    async getPost(_, { PostId }) {
+      try {
+        const post = await Post.findById(PostId);
+        if(post) {
+          return post;
+        } else {
+          throw new Error('Post not found')
+        }
+      } catch(err) {
+        throw new Error(err);
+      }
+    }
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+      console.log(user)
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString()
+      });
+      const post = await newPost.save();
+      return post
+    },
+    async deletePost(_, {PostId}, context) {
+      const user = checkAuth(context);
+      try {
+        const post = await Post.findById(PostId);
+        if(user.username === post.username) {
+          await post.delete();
+          return 'Post deleted'
+        } else {
+          throw new AuthenticationError('Action not allowed')
+        }
+      } catch (err) {
         throw new Error(err);
       }
     }
