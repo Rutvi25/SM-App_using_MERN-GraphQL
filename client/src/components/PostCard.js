1bcb230d18047596b823cc6e229560280a7a9bc6
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-08-03 09:13:22 +0530

    functionality added for like button

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
+import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+
+import LikeButton from './LikeButton';
+import { useSelector } from 'react-redux';
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
-  function likePost() {
-    console.log('likePost');
-  }
-  function commentOnPost() {
-    console.log('comment on post');
-  }
+  const user = useSelector((state) => state.user);
+
   return (
     <Card fluid>
       <Card.Content>
@@ -27,22 +26,25 @@ function PostCard({
         <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <Button as='div' labelPosition='right' onClick={likePost}>
-          <Button color='teal' basic>
-            <Icon name='heart' />
-          </Button>
-          <Label basic color='teal' pointing='left'>
-            {likeCount}
-          </Label>
-        </Button>
-        <Button as='div' labelPosition='right' onClick={commentOnPost}>
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+        <Button labelPosition='right' as={Link} to={`/posts/${id}`}>
           <Button color='blue' basic>
-            <Icon name='comment' />
+            <Icon name='comments' />
           </Button>
           <Label basic color='blue' pointing='left'>
             {commentCount}
           </Label>
         </Button>
+        {user && user.username === username && (
+          <Button
+            as='div'
+            color='red'
+            floated='right'
+            onClick={() => console.log('Delete post')}
+          >
+            <Icon name='trash' style={{ margin: 0 }} />
+          </Button>
+        )}
       </Card.Content>
     </Card>
   );
